185c1ed5510b5a3289c16bb62bbb0732f6f3faf0
Christian Purmann <christian.purmann@smail.inf.h-brs.de>
2023-10-16 22:25:03 +0200

    Cypress Tutorial

---
+++ cypress/integration/firsttest.cy.js
@@ -0,0 +1,13 @@
+//firsttest.js
+
+describe('My First Test', () => {
+
+    it('Launch Browser and Navigate', () => {
+
+        cy.visit('https://www.browserstack.com/');
+
+        cy.title().should('eq', 'Most Reliable App & Cross Browser Testing Platform | BrowserStack')
+
+    })
+
+})
